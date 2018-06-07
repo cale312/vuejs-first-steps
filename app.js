764063838964fae0ab7dad5ace6409a7678ae8de
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-06-07 11:20:25 +0200

    first crappy game using all the methods we just learnt;

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 new Vue({
     el: '#vue-app',
     data: {
-
+        health: 100,
+        ended: false
     },
-    method: {
-
+    methods: {
+        punch: function() {
+            this.health -= 10;
+            (this.health <= 0) ? this.ended = true : false;
+        },
+        restart: function() {
+            this.health = 100;
+            this.ended = false;
+        }
     },
     computed: {
 
