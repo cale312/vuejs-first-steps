63dffffac578d10388755520bb4fcacf291e1e24
Zikisa Cale <zikisacalesogaxa@gmail.com>
2018-06-07 12:01:02 +0200

    using $refs to get elements from the dom

--- app.js
+++ app.js
@@ -1,37 +1,13 @@
-Vue.component('greeting', {
-    template: '<p>Hey I am {{ name }} . <button v-on:click="changeName">Change Name</p>',
-    data: function() {
-        return {
-            name: 'Gor'
-        }
-    },
-    methods: {
-        changeName: function() {
-            this.name = 'Cale'
-        }
-    }
-});
-
 new Vue({
-    el: '#vue-app-one',
+    el: '#vue-app',
     data: {
-
+        output: 'Your fav anime'
     },
     methods: {
-
-    },
-    computed: {
-
-    }
-});
-
-new Vue({
-    el: '#vue-app-two',
-    data: {
-
-    },
-    methods: {
-
+        readRefs: function() {
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
+        }
     },
     computed: {
 
